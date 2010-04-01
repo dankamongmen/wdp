6370e47bd949fcb101f49a1991d2ae67416a6516
Nick Black <dank@qemfd.net>
2010-03-31 23:08:06 -0400

    detail compute mode and compute capability

--- cs8803ss-project/src/cudamemdump.c
+++ cs8803ss-project/src/cudamemdump.c
@@ -1,12 +1,14 @@
 #include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <driver_types.h>
 #include <cuda_runtime_api.h>
 
 // CUDA must already have been initialized before calling cudaid().
 #define CUDASTRLEN 80
 static int
 id_cuda(int dev){
+	struct cudaDeviceProp dprop;
 	int major,minor,attr,cerr;
 	unsigned mem;
 	CUdevice c;
@@ -15,11 +17,13 @@ id_cuda(int dev){
 	if((cerr = cuDeviceGet(&c,dev)) != CUDA_SUCCESS){
 		return -1;
 	}
+	if((cerr = cudaGetDeviceProperties(&dprop,dev)) != CUDA_SUCCESS){
+		return -1;
+	}
 	cerr = cuDeviceGetAttribute(&attr,CU_DEVICE_ATTRIBUTE_WARP_SIZE,c);
 	if(cerr != CUDA_SUCCESS || attr <= 0){
 		return -1;
 	}
-	// FIXME warp/cores is more our "SIMD width" than threads per core...
 	cerr = cuDeviceGetAttribute(&attr,CU_DEVICE_ATTRIBUTE_MULTIPROCESSOR_COUNT,c);
 	if(cerr != CUDA_SUCCESS || attr <= 0){
 		return -1;
@@ -27,7 +31,6 @@ id_cuda(int dev){
 	if((cerr = cuDeviceTotalMem(&mem,c)) != CUDA_SUCCESS){
 		return -1;
 	}
-	// FIXME do something with mem -- new NUMA node?
 	if((cerr = cuDeviceComputeCapability(&major,&minor,c)) != CUDA_SUCCESS){
 		return -1;
 	}
@@ -38,7 +41,15 @@ id_cuda(int dev){
 		free(str);
 		return -1;
 	}
-	printf("%s\n",str);
+	printf("%d.%d %s %s %uMB %s\n",
+		major,minor,
+		dprop.integrated ? "Integrated" : "Standalone",
+		str,mem / (1024 * 1024),
+		dprop.computeMode == CU_COMPUTEMODE_EXCLUSIVE ? "(exclusive)" :
+		dprop.computeMode == CU_COMPUTEMODE_PROHIBITED ? "(prohibited)" :
+		dprop.computeMode == CU_COMPUTEMODE_DEFAULT ? "" :
+		"(unknown compute mode)");
+	free(str);
 	return 0;
 }
 
@@ -58,7 +69,7 @@ init_cuda(void){
 	if((cerr = cuDriverGetVersion(&attr)) != CUDA_SUCCESS){
 		return cerr;
 	}
-	printf("Compiled against CUDA version %d.%d\nLinked against CUDA version %d.%d.\n",
+	printf("Compiled against CUDA version %d.%d. Linked against CUDA version %d.%d.\n",
 			CUDAMAJMIN(CUDA_VERSION),CUDAMAJMIN(attr));
 	if(CUDA_VERSION > attr){
 		fprintf(stderr,"Compiled against a newer version of CUDA than that installed, exiting.\n");
