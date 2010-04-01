f165bf5effd85ebfbd8d7afd2a5a6f192899da50
Nick Black <dank@qemfd.net>
2010-03-31 22:20:59 -0400

    compile in the cuda libraries. initialize cuda up

--- cs8803ss-project/src/cudamemdump.c
+++ cs8803ss-project/src/cudamemdump.c
@@ -1,7 +1,42 @@
 #include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cuda_runtime_api.h>
+
+// CUDA must already have been initialized before calling cudaid().
+#define CUDASTRLEN 80
+#define CORES_PER_NVPROCESSOR 8 //  taken from CUDA 2.3 SDK's deviceQuery.cpp
+static int
+init_cuda(void){
+	int attr,count;
+	CUresult cerr;
+	/*
+	CUdevice c;
+	char *str;
+	*/
+
+	if((cerr = cuInit(0)) != CUDA_SUCCESS){
+		/*if(cerr == CUDA_ERROR_NO_DEVICE){
+			return 0;
+		}*/
+		return cerr;
+	}
+	if((cerr = cuDriverGetVersion(&attr)) != CUDA_SUCCESS){
+		return cerr;
+	}
+	if((cerr = cuDeviceGetCount(&count)) != CUDA_SUCCESS){
+		return cerr;
+	}
+	return 0;
+}
 
 int main(void){
+	int err;
+
+	if( (err = init_cuda()) ){
+		fprintf(stderr,"Error initializing CUDA (%s?)\n",
+				cudaGetErrorString(err));
+		return EXIT_FAILURE;
+	}
 	return EXIT_SUCCESS;
 }
