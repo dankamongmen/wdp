9d3f2df3ca9452a8c2d27cbd5eec71ea40a8e2d9
Nick Black <dank@qemfd.net>
2010-03-31 22:25:49 -0400

    do proper CUDA version checks; extract device count

--- cs8803ss-project/src/cudamemdump.c
+++ cs8803ss-project/src/cudamemdump.c
@@ -24,9 +24,20 @@ init_cuda(void){
 	if((cerr = cuDriverGetVersion(&attr)) != CUDA_SUCCESS){
 		return cerr;
 	}
+	printf("Compiled against CUDA version %d\nLinked against CUDA version %d\n",
+			CUDA_VERSION,attr);
+	if(CUDA_VERSION > attr){
+		fprintf(stderr,"Compiled against a newer version of CUDA than that installed, exiting.\n");
+		return -1;
+	}
 	if((cerr = cuDeviceGetCount(&count)) != CUDA_SUCCESS){
 		return cerr;
 	}
+	if(count == 0){
+		fprintf(stderr,"No CUDA devices found, exiting.\n");
+		return -1;
+	}
+	printf("CUDA device count: %d\n",count);
 	return 0;
 }
 
@@ -34,8 +45,10 @@ int main(void){
 	int err;
 
 	if( (err = init_cuda()) ){
-		fprintf(stderr,"Error initializing CUDA (%s?)\n",
-				cudaGetErrorString(err));
+		if(err > 0){
+			fprintf(stderr,"Error initializing CUDA (%s?)\n",
+					cudaGetErrorString(err));
+		}
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
