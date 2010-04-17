79cfef2ebe875e985bf1ed5dbef4dba627b502e3
Nick Black <dank@qemfd.net>
2010-04-17 12:41:32 -0400

    take a size argument in cudaspawner

--- cs8803ss-project/src/cudaspawner.cu
+++ cs8803ss-project/src/cudaspawner.cu
@@ -12,18 +12,25 @@ static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 static int
-init_thread(CUcontext *pctx,CUdevice dev){
+init_thread(CUcontext *pctx,CUdevice dev,size_t s){
+	CUdeviceptr ptr;
 	CUresult cerr;
 
 	if( (cerr = cuCtxCreate(pctx,0,dev)) ){
 		fprintf(stderr," Error (%d) creating CUDA context\n",cerr);
 		return -1;
 	}
-	printf("Got context at %p\n",*pctx);
+	if(s){
+		if( (cerr = cuMemAlloc(&ptr,s)) ){
+			fprintf(stderr," Error (%d) allocating %zub\n",cerr,s);
+			return -1;
+		}
+	}
 	return 0;
 }
 
 typedef struct ctx {
+	size_t s;
 	CUdevice dev;
 	unsigned threadno;
 } ctx;
@@ -33,16 +40,16 @@ thread(void *unsafectx){
 	ctx x = *(ctx *)unsafectx;
 	CUcontext cu;
 
-	if(init_thread(&cu,x.dev)){
+	if(init_thread(&cu,x.dev,x.s)){
 		goto err;
 	}
 	pthread_mutex_lock(&lock);
+	printf("Got context at %p\n",cu);
 	thrdone = 1;
 	pthread_cond_broadcast(&cond);
 	while(threadsmaintain){
 		pthread_cond_wait(&cond,&lock);
 	}
-	printf("Thread %ju exiting!\n",x.threadno);
 	pthread_mutex_unlock(&lock);
 	return NULL;
 
@@ -59,7 +66,7 @@ err:
 // since the latter are unsafe across hardware removal/additions.
 static void
 usage(const char *a0){
-	fprintf(stderr,"usage: %s devno\n",a0);
+	fprintf(stderr,"usage: %s devno perthreadbytes\n",a0);
 }
 
 static int
@@ -98,15 +105,19 @@ init_cuda(int devno,CUdevice *c){
 }
 
 int main(int argc,char **argv){
-	unsigned total = 0;
 	unsigned long zul;
+	unsigned total = 0;
 	ctx marsh;
 
-	if(argc != 2){
+	if(argc != 3){
 		usage(argv[0]);
 		exit(EXIT_FAILURE);
 	}
 	if(get_devno(argv[0],argv[1],&zul)){
+		usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	if(get_devno(argv[0],argv[2],&marsh.s)){
 		usage(argv[0]);
 		exit(EXIT_FAILURE);
 	}
