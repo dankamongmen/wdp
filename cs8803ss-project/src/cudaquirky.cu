26f23e699fac5f6c8b2cd5970520cae0e4e1d19f
Nick Black <dank@qemfd.net>
2010-04-22 17:13:52 -0400

    call and synchronize

--- cs8803ss-project/src/cudaquirky.cu
+++ cs8803ss-project/src/cudaquirky.cu
@@ -6,13 +6,20 @@ __global__ void quirkykernel(void){
 
 	sharedvar = 0;
 	__syncthreads();
-	while(sharedvar != threadIdx.x);
-		/***reconvergencepoint***/
-	sharedvar++;
+	while(sharedvar != threadIdx.x) /*** reconvergencepoint ***/ ;
+	++sharedvar;
 }
 
 int main(void){
+	dim3 dg(1,1),db(64,1,1);
+	CUresult cerr;
+
 	if(init_cuda(0,NULL)){
+		return EXIT_FAILURE;
+	}
+	quirkykernel<<<dg,db>>>();
+	if((cerr = cuCtxSynchronize()) != CUDA_SUCCESS){
+		fprintf(stderr,"Error running kernel (%d)\n",cerr);
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
