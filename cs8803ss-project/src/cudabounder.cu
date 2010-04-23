3db9a48bffe1c8b83d34cc7c2bd030a3e3492050
Nick Black <dank@qemfd.net>
2010-04-23 11:02:30 -0400

    implement most of cudabounder

--- cs8803ss-project/src/cudabounder.cu
+++ cs8803ss-project/src/cudabounder.cu
@@ -8,18 +8,6 @@ usage(const char *a0){
 	fprintf(stderr,"usage: %s devno\n",a0);
 }
 
-__global__ void
-touchbytes(CUdeviceptr ptr,uint32_t off,CUdeviceptr res){
-	uint8_t b;
-
-	b = *(unsigned char *)((uintptr_t)ptr + off + blockIdx.x);
-	if(b == 0xff){
-		*(uint32_t *)((uintptr_t)res + blockIdx.x) = 1;
-	}
-}
-
-#define BYTES_PER_KERNEL 4
-
 static int
 basic_params(CUdeviceptr p,size_t s){
 	CUdeviceptr p2;
@@ -39,12 +27,24 @@ basic_params(CUdeviceptr p,size_t s){
 	return 0;
 }
 
+#define BYTES_PER_KERNEL 4
+
+__global__ void
+touchbytes(CUdeviceptr ptr,uint32_t off,CUdeviceptr res){
+	uint8_t b;
+
+	b = *(unsigned char *)((uintptr_t)ptr + off + blockIdx.x);
+	if(b == 0xff){
+		*(uint32_t *)((uintptr_t)res + blockIdx.x * BYTES_PER_KERNEL) = 1;
+	}
+}
+
 int main(int argc,char **argv){
 	CUdeviceptr ptr,res;
 	unsigned long zul;
 	CUcontext ctx;
 	CUresult cerr;
-	size_t s;
+	size_t s,z;
 
 	if(argc != 2 || getzul(argv[1],&zul)){
 		usage(*argv);
@@ -67,6 +67,23 @@ int main(int argc,char **argv){
 		fprintf(stderr,"Couldn't alloc+init %zu base (%d)\n",s,cerr);
 		exit(EXIT_FAILURE);
 	}
+	if(res <= ptr){ // FIXME...see loop detect below
+		fprintf(stderr,"Unexpected pointer arrangement (%p >= %p)\n",ptr,res);
+		exit(EXIT_FAILURE);
+	}
 	printf("Got result %zub allocation at %p\n",BYTES_PER_KERNEL * sizeof(uint32_t),res);
+	z = 0;
+	while((cerr = cuCtxSynchronize()) == CUDA_SUCCESS){
+		dim3 dg(1,1,1),db(BYTES_PER_KERNEL,1,1);
+
+		printf("running on %zu\n",z);
+		touchbytes<<<dg,db>>>(ptr,z,res);
+		// FIXME check res
+		if(((z += BYTES_PER_KERNEL) + ptr) > res){
+			printf("Hit result array at %p; breaking loop\n",res);
+			break;
+		}
+	}
+	printf("Exited loop (ret: %d) at %zu\n",cerr,z);
 	exit(EXIT_SUCCESS);
 }
