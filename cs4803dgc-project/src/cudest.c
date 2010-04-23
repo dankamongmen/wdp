76f4ef37a9329775127cf3a7e4b1156535939345
Nick Black <dank@qemfd.net>
2010-04-22 21:21:18 -0400

    got the first one worked out! YES!

--- cs4803dgc-project/src/cudest.c
+++ cs4803dgc-project/src/cudest.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <sys/ioctl.h>
 
 #define NVCTLDEV "/dev/nvidiactl"
@@ -15,7 +16,7 @@ typedef enum {
 static int nvctl = -1;
 
 typedef struct nvfifo { // FIXME just a placeholding guess
-	char ob[0x48];
+	uint64_t ob[9];	// 0x48 bytes
 } nvfifo;
 
 static CUresult
@@ -23,6 +24,8 @@ init_ctlfd(int fd){
 	nvfifo fifodesc;
 
 	memset(&fifodesc,0,sizeof(fifodesc));
+	fifodesc.ob[2] = 0x35ull;
+	fifodesc.ob[1] = 0x312e36332e353931ull;
 	if(ioctl(fd,NV_PREPARE_FIFO,&fifodesc)){
 		return CUDA_ERROR_INVALID_DEVICE;
 	}
