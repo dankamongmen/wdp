5a53b23d9223e7b09b1219dba445d5a6e06d647f
Nick Black <dank@qemfd.net>
2010-04-29 20:28:07 -0400

    Revert "presentation plan"
    This reverts commit 84469e10febd09f85ab676a47475988abe6cff8b.

--- cs8803ss-project/src/cudaminimal.c
+++ cs8803ss-project/src/cudaminimal.c
@@ -6,10 +6,38 @@
 #include <string.h>
 #include "cuda8803ss.h"
 
-int main(void){
+static void
+usage(const char *argv){
+	fprintf(stderr,"usage: %s [ signo ]\n",argv);
+}
+
+int main(int argc,char **argv){
+	unsigned long zul;
 	CUresult cerr;
 
-	cerr = cuInit(0);
+	if(argc > 2){
+		usage(*argv);
+		exit(EXIT_FAILURE);
+	}else if(argc == 2){
+		if(getzul(argv[1],&zul)){
+			usage(*argv);
+			exit(EXIT_FAILURE);
+		}
+	}
+	if( (cerr = cuInit(0)) ){
+		fprintf(stderr,"Couldn't initialize CUDA (%d)\n",cerr);
+		exit(EXIT_FAILURE);
+	}
+	printf("CUDA initialized.\n");
+	if(argc == 2){
+		sigset_t s;
+		int sig;
 
-	exit(cerr);
+		if(sigemptyset(&s) || sigaddset(&s,zul) || sigwait(&s,&sig)){
+			fprintf(stderr,"Error waiting for signal %lu (%s?)\n",
+					zul,strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+	}
+	exit(EXIT_SUCCESS);
 }
